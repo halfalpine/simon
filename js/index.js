8dfe288a6095125f6f5e7be0b93c84df34943a68
halfalpine <rocksandwoks@gmail.com>
2016-08-11 20:23:43 -0400

    Initial migration from CodePen

---
+++ js/index.js
@@ -0,0 +1,165 @@
+$(".document").ready(function() {
+  
+  let data = {
+    sequence: null,
+    compSeq: [],
+    humanSeq: [],
+    ms: [1200, 1000, 800],
+    colors: [green, red, yellow, blue],
+    turns: 0
+  };
+  
+  let controller = (function(){
+    
+    function init() {
+      view.init();
+    }
+    
+    function play() {
+      data.compSeq.push(data.sequence[data.turns]);
+      data.turns++;
+      console.log("compSeq", data.compSeq, "turns", data.turns);
+      // Check for winner, call victory() and return true
+      simon.playSeq(data.compSeq);
+    }
+    
+    function start() {
+      data.sequence = simon.sequence;
+      controller.play();
+    }
+    
+    function strict() {
+      $('#strict-display').toggleClass('strict-on');
+    }
+    
+    publicAPI = {
+      init: init,
+      play: play,
+      start: start,
+      strict: strict
+    }
+      
+    return publicAPI;
+  }());
+ 
+  // This function should togggle all event listeners on and off
+  let events = (function() {
+    var activateEvents = function() {
+      // $('#test').on('click', controller.init);
+      $('#start-button').on('mousedown', controller.start);
+      $('#strict-button').on('click', controller.strict);
+      $('#green').on('mousedown', press.green);
+      $('#red').on('mousedown', press.red);
+      $('#yellow').on('mousedown', press.yellow);
+      $('#blue').on('mousedown', press.blue);
+    };
+    
+    var deactivateEvents = function() {
+      $('#test').off('click', controller.init);
+      $('#start-button').off('click', controller.start);
+      $('#strict-button').off('click', controller.strict);
+      $('#green').off('mousedown', press.green);
+      $('#red').off('mousedown', press.red);
+      $('#yellow').off('mousedown', press.yellow);
+      $('#blue').off('mousedown', press.blue);
+    };
+    
+    let togglePower = function() {
+      this.checked ? activateEvents() : deactivateEvents();
+    };
+    
+    return togglePower;
+  }());
+  
+  // The 'simon' object contains helper functions
+  let simon = {
+   
+    hasWinner: function(humArr, compArr){
+      return humArr.every((value, index) => compArr[index] === humArr[index]);
+    },
+    
+    translateToColor: function(num) {
+      return data.colors[num];
+    },
+    
+    playSeq: function(arr) { // arr is data.compSeq
+      arr.each(function(value, index) {
+        
+        // Light up button
+        // Make sound
+        
+          // Color down button
+      });
+    },
+    
+    sequence: (function() {
+      //Return an array of 20 numbers between 0 and 3
+      let arr = new Array(20)
+      for (let i = 0; i < arr.length; i++) {
+        let rand = Math.floor(Math.random() * 4);
+        arr[i] = rand;
+      }
+      return arr;
+    }()),
+    
+    regGame: (function() {
+      if (data.humanMoves > 20) {
+        simon.victory;
+        return;
+      } else {
+        
+      }
+    }())
+  }
+  
+  //A collection of buttons
+  let press = {
+    green: function() {
+      let that = this;
+      let greenSound = document.getElementById('green-sound');
+      let newSound = greenSound.cloneNode();
+      newSound.play();
+      $(this).addClass('press');
+      setTimeout(() => {press.removeOpacity(that)}, 200);
+    },
+    
+    red: function() {
+      let that = this;
+      let redSound = document.getElementById('red-sound');
+      let newSound = redSound.cloneNode();
+      newSound.play();
+      $(this).addClass('press');
+      setTimeout(() => {press.removeOpacity(that)}, 200)
+    },
+    
+    yellow: function() {
+      let that = this;
+      let yellowSound = document.getElementById('yellow-sound');
+      let newSound = yellowSound.cloneNode();
+      newSound.play();
+      $(this).addClass('press');
+      setTimeout(() => {press.removeOpacity(that)}, 200)
+    },
+    
+    blue: function() {
+      let that = this;
+      let blueSound = document.getElementById('blue-sound');
+      let newSound = blueSound.cloneNode();
+      newSound.play();
+      $(this).addClass('press');
+      setTimeout(() => {press.removeOpacity(that)}, 200)
+    },
+    
+    removeOpacity: function(x) {
+      $(x).removeClass('press').bind(press.green);
+    }
+  };
+  
+  let view = {
+    init: function() {
+      $('#power').change(events);
+    }
+  };
+  
+  controller.init();
+});
